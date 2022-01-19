17573b6c1ada12bee83139f6f130486c184caeae
Peter Milne <milnepe@gmail.com>
2022-01-19 13:47:05 +0000

    Update to assimp-5.0.1

--- lidar_ros_setup.sh
+++ lidar_ros_setup.sh
@@ -59,62 +59,83 @@ PYTHON_DEPS="python-rosdep python-rosinstall-generator python-wstool python-rosi
 echo "Adding ros repo https://www.ros.org/"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-#wget -qO - http://packages.ros.org/ros.key | sudo apt-key add -
 
-echo "Install dependencies"
 sudo apt update
 sudo apt install -y $BUILD_DEPS $PYTHON_DEPS
+pwd
+echo "Press space bar to continue"
+read -r -s -d ' '
 
-# Note remove /etc/ros/rosdep/sources.list.d/20-default.list
-# to re-run initialisation
-echo "Initialising ROS"
-sudo rosdep init
+echo "STEP2: Initialising ROS"
+if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+    sudo rosdep init
+fi
 rosdep update
-
-echo "STEP2(OPTIONAL if necessary): Solve the ERROR: Edit script"
-#ERROR :cannot download default sources list from: https://raw.githubusercontent.com/ros/rosdistro/master/rosdep/sources.list.d/20-default.list Website may be down.
-
-#sudo vim /etc/hosts
-#Add 151.101.76.133    raw.githubusercontent.com
-#Then reinitialize rosdep and update it
-#sudo rosdep init
-#rosdep update
-
+pwd
+echo "Press space bar to continue"
+read -r -s -d ' '
 
 echo "STEP3: Install Melodic Desktop"
-mkdir $WORK_DIR
+if [ ! -d "$WORK_DIR" ]; then
+    mkdir "$WORK_DIR"
+fi
 cd $WORK_DIR
 
 rosinstall_generator desktop --rosdistro melodic --deps --wet-only --tar > melodic-desktop-wet.rosinstall
-wstool init -j8 src melodic-desktop-wet.rosinstall
-
-echo "If wstool init fails or is interrupted, you can resume the download by running:"
-#wstool update -j4 -t src
+if [ ! -d "$WORK_DIR"/src ]; then
+    wstool init -j8 src melodic-desktop-wet.rosinstall
+else
+    #echo "If wstool init fails or is interrupted, you can resume the download by running:"
+    wstool update -j4 -t src
+fi
+pwd
+echo "Press space bar to continue"
+read -r -s -d ' '
 
 echo "STEP4: Fix the Issues"
 # Install compatible version of Assimp (Open Asset Import Library) to fix collada_urdf dependency problem.
-mkdir $WORK_DIR/external_src
-cd $WORK_DIR/external_src
-wget http://sourceforge.net/projects/assimp/files/assimp-3.1/assimp-3.1.1_no_test_models.zip/download -O assimp-3.1.1_no_test_models.zip
-unzip assimp-3.1.1_no_test_models.zip
-cd assimp-3.1.1
+if [ ! -d "$WORK_DIR"/external_src ]; then
+    mkdir "$WORK_DIR"/external_src
+fi
+
+cd "$WORK_DIR/external_src"
+
+wget https://github.com/assimp/assimp/archive/refs/tags/v5.0.1.zip
+unzip -o v5.0.1.zip
+cd assimp-5.0.1
 cmake .
 make
 sudo make install
+pwd
+echo "Press space bar to continue"
+read -r -s -d ' '
 
 echo "Install OGRE for rviz"
 sudo apt install -y libogre-1.9-dev
+pwd
+echo "Press space bar to continue"
+read -r -s -d ' '
 
 cd $WORK_DIR
 
 echo "Install other deps using rosdep"
 rosdep install -y --from-paths src --ignore-src --rosdistro melodic -r --os=debian:buster
+pwd
+echo "Press space bar to continue"
+read -r -s -d ' '
 
 echo "STEP5: Build and Source the Installation"
+cd $WORK_DIR
+
 sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/melodic -j2
+pwd
+echo "Press space bar to continue"
+read -r -s -d ' '
 
 echo "Update bashrc"
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+if ! grep -xq 'source /opt/ros/melodic/setup.bash' ~/.bashrc; then
+	echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+fi
 
 # *************************
 # End of ROS build
